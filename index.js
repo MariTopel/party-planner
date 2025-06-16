98ca457ebc598076e00a2a8228fccb323a908a66
Mari Topel <maritopel@gmail.com>
2025-06-16 12:27:08 -0500

    finished the fetchPartyByID async function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 // made a variable for the api so that i can use it easier when coding
-const API = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2504-FTB-ET-WEB-PT/events';
+const API =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2504-FTB-ET-WEB-PT/events";
 
 // made an object that holds the parties array and selected party.
 const state = {
@@ -21,12 +21,13 @@ async function fetchParties() {
 }
 
 // made async function to be able to fetch the selected party by id and used later
-async function fetchParyById(id) {
-    try {
-        const response = await fetch(`${API}/${id}`)
-    }
+async function fetchPartyById(id) {
+  try {
+    const response = await fetch(`${API}/${id}`);
+    const result = await response.json();
+    state.pickOneParty = result.data;
+    render();
+  } catch (error) {
+    console.error("could not retrieve single party", error);
+  }
 }
-
-
-
-const app = document.getElementById('app');
